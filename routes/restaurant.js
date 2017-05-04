ef909bfd762f88f8210e009086ee18bda5dc84d4
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-04 11:36:05 +0700

    adding restaurant route done

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -4,6 +4,17 @@ const passport = require('passport');
 const restaurant = require('../controllers/restaurant');
 const isLogin = require('../helpers/auth');
 
+// create
+router.post('/create', restaurant.create);
 
+// get route
+router.get('/', restaurant.getAll);
+router.get('/:name', restaurant.findByName);
+
+// edit
+router.put('/:name', restaurant.edit);
+
+// delete
+router.delete('/:name', restaurant.delete);
 
 module.exports = router;
