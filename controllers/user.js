d11716f4eb97cbcb7de8bb5f208d9e15f1737c44
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-04 10:51:49 +0700

    adding user sign up controller

---
+++ controllers/user.js
@@ -0,0 +1,16 @@
+const User = require('../models/user');
+const passwordHash = require('password-hash');
+const jwt = require('jsonwebtoken');
+const methods = {};
+
+methods.signup = (req, res, next) => {
+  User.create({
+    username: req.body.username,
+    password: passwordHash.generate(req.body.password),
+  }, (err, done) => {
+    if(err) res.send(err);
+    res.send(done);
+  })
+}
+
+module.exports = methods;
